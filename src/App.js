a6a79d287e8f6bf2f7d6a095cd46dbee6e03291d
casey cole <casey@capsinc.com>
2022-04-10 18:06:46 -0400

    added scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <div className="App">Hello World</div>
+import { useState } from "react"
+import "./categories.styles.scss"
+const App = () => {
+  const [categorys] = useState([
+    { title: "Hats" },
+    { title: "Sneakers" },
+    { title: "Jackets" },
+    { title: "Mens" },
+    { title: "Womens" }
+  ])
+  return (
+    <div className="categories-container">
+      {categorys.map(({ title }) => (
+        <div key={title} className="category-container">
+          <div className="background-image"></div>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default App
