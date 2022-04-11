a5d0dcf6d6a205c7310bc62355926a49020ac557
casey cole <casey@capsinc.com>
2022-04-11 16:37:47 -0400

    added category component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import { useState } from "react"
 import "./categories.styles.scss"
+import { CategoryItem } from "./components/category-item/category-item.component"
 const App = () => {
   const [categorys] = useState([
-    { title: "Hats" },
-    { title: "Sneakers" },
-    { title: "Jackets" },
-    { title: "Mens" },
-    { title: "Womens" }
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ])
   return (
     <div className="categories-container">
-      {categorys.map(({ title }) => (
-        <div key={title} className="category-container">
-          <div className="background-image"></div>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categorys.map(category => (
+        <CategoryItem key={category.title} category={category} />
       ))}
     </div>
   )
