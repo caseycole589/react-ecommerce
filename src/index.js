2a3356cff1d6e1822f8bc3ec254d734435063e58
casey cole <casey@capsinc.com>
2022-04-12 16:34:29 -0400

    moved folder that was in wrong directory

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import "./index.scss"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 
 const root = createRoot(document.getElementById("root"))
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 )
