66eca70b3f32df5be323e0aa5aaa36f53edb5742
casey cole <casey@capsinc.com>
2022-04-18 16:37:53 -0400

    added form inputs

--- src/components/sign-up/sign-up-form.component.jsx
+++ src/components/sign-up/sign-up-form.component.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react"
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  comfirmPassword: ""
+}
+
 export const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields)
+
+  const handleChange = evt => {
+    const { name, value } = evt.target
+    setFormFields({ ...formFields, [name]: value })
+  }
   return (
     <div>
       <h1>Sign Up With Email & Password</h1>
       <form onSubmit={() => {}}>
         <label>Display Name</label>
-        <input type="text" required />
+        <input type="text" required onChange={handleChange} name="displayName" value={formFields.displayName} />
         <label>Email</label>
-        <input type="email" required />
+        <input type="email" required onChange={handleChange} name="email" value={formFields.email} />
         <label>Password</label>
-        <input type="password" required />
+        <input type="password" required onChange={handleChange} name="password" value={formFields.password} />
         <label>Confirm Password</label>
-        <input type="password" required />
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={formFields.comfirmPassword}
+        />
         <button type="submit"></button>
       </form>
     </div>
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react"
-import { getRedirectResult } from "firebase/auth"
+// import { useEffect } from "react"
+// import { getRedirectResult } from "firebase/auth"
 import {
-  auth,
+  // auth,
   signInWithGooglePopup,
-  createUserDocumentFromAuth,
-  signInWithGoogleRedirect
+  createUserDocumentFromAuth
+  // signInWithGoogleRedirect
 } from "../../utils/firebase/firebase.utils"
+import { SignUpForm } from "../../components/sign-up/sign-up-form.component"
 export const SignIn = () => {
   // useEffect(() => {
   //   async function checkAuth(params) {
@@ -28,6 +29,7 @@ export const SignIn = () => {
     <div>
       <h1>sign in</h1>
       <button onClick={logGoogleUser}>Sign In WIth Google</button>
+      <SignUpForm />
       {/* <button onClick={signInWithGoogleRedirect}>Sign In WIth Google Redirect</button> */}
     </div>
   )
