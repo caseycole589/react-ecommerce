3b6ea07891a0874af170443d72c8685af544e37e
casey cole <casey@capsinc.com>
2022-04-24 16:04:35 -0400

    finished the cart components still need logic to handle adding and removing items

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,13 @@
+import { CartContext } from "../../contexts/cart.context";
+import { useContext } from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 
 export const CartIcon = () => {
+	const { cartOpen, setCartOpen } = useContext(CartContext);
+	const handleClick = () => setCartOpen(!cartOpen);
 	return (
-		<div className="cart-icon-container">
+		<div className="cart-icon-container" onClick={handleClick}>
 			<ShoppingIcon className="shopping-icon" />
 			<span className="item-count">0</span>
 		</div>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,11 +4,13 @@ import { ReactComponent as Logo } from "../../assets/logo.svg";
 import { CartDropdown } from "../../components/cart-dropdown/cart-dropdown.component";
 import { CartIcon } from "../../components/cart-icon/cart-icon.component";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
 export const Navigation = () => {
 	//use context is a hook that causes component to rerender any time there is a change
 	const { currentUser } = useContext(UserContext);
+	const { cartOpen } = useContext(CartContext);
 	return (
 		<Fragment>
 			<div className="navigation">
@@ -30,7 +32,7 @@ export const Navigation = () => {
 					)}
 					<CartIcon />
 				</div>
-				<CartDropdown />
+				{cartOpen && <CartDropdown />}
 			</div>
 			<Outlet />
 		</Fragment>
