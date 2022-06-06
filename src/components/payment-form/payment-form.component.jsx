41fec2aa055cecd9b96aa410b5fc58c0748edcf4
casey cole <casey@capsinc.com>
2022-06-06 17:21:38 -0400

    netifly function

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -6,10 +6,27 @@ export const PaymentForm = () => {
 	const elements = useElements();
 	const paymentHandler = async (e) => {
 		e.preventDefault();
+
+		if (!stripe || !elements) {
+			return;
+		}
+
+		const response = await fetch(
+			"/.netlify/functions/create-payment-intent",
+			{
+				method: "post",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ amount: 10000 }),
+			}
+		).then((resp) => resp.json());
+
+		console.log(response);
 	};
 	return (
 		<PaymentFormContainer>
-			<FormContainer>
+			<FormContainer onSubmit={paymentHandler}>
 				<h2>Credit Card Payment:</h2>
 				<CardElement />
 				<Button>Pay Now</Button>
