a5199e9605ea89cb9643c482352e08b516856267
casey cole <casey@capsinc.com>
2022-06-18 18:08:28 -0400

    rip out persist or was not getting any benefits

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "./index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
 // import reportWebVitals from "./reportWebVitals";
-import { PersistGate } from "redux-persist/integration/react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { store, persistor } from "./store/store";
@@ -14,13 +13,11 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <BrowserRouter>
-          <Elements stripe={stripePromise}>
-            <App />
-          </Elements>
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
