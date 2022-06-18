a99281a4c68150fc53643f8674f16804e54356f4
casey cole <casey@capsinc.com>
2022-06-18 17:13:38 -0400

    swap out complicated create action

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,24 +1,4 @@
-import { AnyAction } from "redux";
-
-export type ActionWithPayload<T, P> = {
-	type: T;
-	payload?: P;
-};
-
-export type Action<T> = {
-	type: T;
-};
-
-export function createAction<T extends string, P>(
-	type: T,
-	payload: P
-): ActionWithPayload<T, P>;
-
-export function createAction<T extends string, P>(
-	type: T,
-	payload: void
-): Action<T>;
-
-export function createAction<T extends string, P>(type: T, payload: P) {
-	return { type, payload };
-}
+export const createAction = (type: string, payload?: any) => ({
+	type,
+	payload,
+});
