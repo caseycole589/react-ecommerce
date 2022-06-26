cd917ae1f733535238ef7c2e29f00fb35a2f3546
casey cole <casey@capsinc.com>
2022-06-26 15:54:06 -0400

    started on making wish list component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import { Authentication } from "./routes/authentication/authentication.component";
 import { Checkout } from "./routes/checkout/checkout.component";
 import { Home } from "./routes/home/home.component";
+import { WishList } from "./routes/wishlist/wishlist.component";
 import { Navigation } from "./routes/navigation/navigation.component";
 import { Shop } from "./routes/shop/shop.component";
 import { setCurrentUser } from "./store/user/user.action";
@@ -35,6 +36,7 @@ export const App = () => {
         <Route path="shop/*" element={<Shop />} />
         <Route path="checkout" element={<Checkout />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="wishlist" element={<WishList />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/wishlist/wishlist.component.tsx
@@ -0,0 +1,7 @@
+export const WishList = () => {
+	return (
+		<div>
+			<div>Hellow WOrld</div>
+		</div>
+	);
+};
