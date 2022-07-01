bc6d64f11fc8c38880b7ef531ca3eb8ecf38e559
casey cole <casey@capsinc.com>
2022-07-01 18:10:02 -0400

    on small screen add slide out for links have cart link directly to cart page

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
+import {} from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 //@ts-ignore
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 import { CartDropdown } from "../../components/cart-dropdown/cart-dropdown.component";
@@ -13,7 +14,7 @@ export const Navigation = () => {
 	//use context is a hook that causes component to rerender any time there is a change
 	const currentUser = useSelector(selectCurrentUser);
 	const cartOpen = useSelector(selectCartOpen);
-
+	const navigate = useNavigate();
 	return (
 		<Fragment>
 			<div className="navigation">
@@ -21,19 +22,51 @@ export const Navigation = () => {
 					<Logo className="logo" />
 				</Link>
 				<div className="nav-links-container">
-					<Link className="nav-link" to="/shop">
-						Shop
-					</Link>
+					<div id="shopLink" className="nav-link">
+						<span>Shop</span>
+						<div className="shop-dropdown">
+							<div
+								onClick={() => navigate("/shop/hats")}
+								className="shop-dropdown-link"
+							>
+								Hats
+							</div>
+							<div
+								onClick={() => navigate("/shop/jackets")}
+								className="shop-dropdown-link"
+							>
+								Jackets
+							</div>
+							<div
+								onClick={() => navigate("/shop/mens")}
+								className="shop-dropdown-link"
+							>
+								Mens
+							</div>
+							<div
+								onClick={() => navigate("/shop/womens")}
+								className="shop-dropdown-link"
+							>
+								Womens
+							</div>
+							<div
+								className="shop-dropdown-link"
+								onClick={() => navigate("/shop/sneakers")}
+							>
+								Sneaker
+							</div>
+						</div>
+					</div>
 					<Link className="nav-link" to="/wishlist">
-						Wish List
+						<span>Wish List</span>
 					</Link>
 					{currentUser ? (
 						<span className="nav-link" onClick={signOutUser}>
-							Sign Out
+							<span>Sign Out</span>
 						</span>
 					) : (
 						<Link className="nav-link" to="/auth">
-							Sign In
+							<span>Sign In</span>
 						</Link>
 					)}
 					<CartIcon />
