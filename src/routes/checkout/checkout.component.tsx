2e6ce184ba9cb7e2e64ff6bc0f7ad90a671bde8c
casey cole <casey@capsinc.com>
2022-07-28 12:08:03 -0400

    finally finished all the check out css

--- src/routes/checkout/checkout.component.tsx
+++ src/routes/checkout/checkout.component.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { CheckoutItem } from "../../components/checkout-item/checkout-item.component";
 import {
 	selectCartItems,
@@ -8,12 +9,27 @@ import "./checkout.styles.scss";
 export const Checkout = () => {
 	const cartTotal = useSelector(selectCartTotal);
 	const cartItems = useSelector(selectCartItems);
+	const handleCheckoutClick = () => {};
 	return (
 		<div className="checkout-container">
 			{cartItems.map((item) => (
 				<CheckoutItem key={item.id} product={item} />
 			))}
-			<span className="total">Total: ${cartTotal}</span>
+			<div className="total-container">
+				<div className="total-text-container">
+					<span className="total">Total: ${cartTotal}</span>
+				</div>
+				<div>
+					<Link className="nav-link" to="/checkout">
+						<button
+							onClick={handleCheckoutClick}
+							className="checkout-button"
+						>
+							Checkout
+						</button>
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
